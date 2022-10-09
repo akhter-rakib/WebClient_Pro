6401e0316e82d1f44ac2251f448f40ce8929996e
rakib <rakibccj@gmail.com>
2022-10-09 23:03:17 +0600

    AppProperties class update with @Configuration annotation

--- src/main/java/com/rakib/webclient/config/AppProperties.java
+++ src/main/java/com/rakib/webclient/config/AppProperties.java
@@ -3,8 +3,9 @@ package com.rakib.webclient.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
     private final GitHub github = new GitHub();
